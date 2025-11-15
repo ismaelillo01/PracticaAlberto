8c63c1a52379d0d8de97b47fff82d01c2c3f6d71
ismaelillo01 <ismael.frayan@educa.jcyl.es>
2025-11-15 01:55:25 +0100

    mentira no entiendo nada

--- ProyectoAlberto/src/main/java/Configuracion.java
+++ ProyectoAlberto/src/main/java/Configuracion.java
@@ -2,23 +2,34 @@ import java.io.*;
 
 public class Configuracion implements Serializable {
     private String urlFeed;
+    private static final String PATH="../data/config.ser";
+
+
+    public Configuracion(){
+        this.urlFeed="https://www.xataka.com/feedburner.xml";
+    }
 
     public void guardarConfig(){
         try {
-            File fichero=new File("../data/config.ser");
-            FileOutputStream fos=new FileOutputStream(fichero);
-            ObjectOutputStream oos= new ObjectOutputStream(fos);
+            ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(PATH));
+            oos.writeObject(this);
+            oos.close();
+            System.out.println("Configuracion guardada en "+PATH);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-    public void cargarConfig(){
+    public static Configuracion cargarConfig(){
         try {
-            File fichero=new File("../data/config.ser");
-            FileInputStream fis=new FileInputStream(fichero);
-            ObjectInputStream ois= new ObjectInputStream(fis);
+            ObjectInputStream ois=new ObjectInputStream(new FileInputStream(PATH));
+            Configuracion config=(Configuracion)ois.readObject();
+            ois.close();
+            System.out.println("Configuracion cargada desde "+PATH);
+            return config;
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
